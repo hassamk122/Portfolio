f3dd507af4998355a7087e2ed449c1601ff149de
hassamk122 <hassamkiani66@gmail.com>
2025-03-01 16:31:09 +0500

    changed 404 color

--- src/Pages/Notfound.jsx
+++ src/Pages/Notfound.jsx
@@ -4,10 +4,10 @@ import { motion} from "motion/react";
 
 function Notfound() {
   return (
-    <div className=' flex items-center justify-center flex-col text-4xl h-screen text-zinc-800 font-Excalifont text-center py-40 overflow-hidden'>
+    <div className=' flex items-center justify-center flex-col text-4xl h-screen text-zinc-800 dark:text-zinc-400 font-Excalifont text-center py-40 overflow-hidden'>
         <motion.img initial={{x:200}} animate={{x:0}}  transition={{duration:2.4}}
         src={ballon} className='w-40 sm:w-60 p-2' alt='ballon-loading-animation'></motion.img>
-        <h1 className='text-6xl sm:text-8xl'>404</h1><small className='text-sm sm:text-base'>The requested resource could not be found!</small></div>
+        <h1 className='text-6xl sm:text-8xl '>404</h1><small className='text-sm sm:text-base'>The requested resource could not be found!</small></div>
   )
 }
 
