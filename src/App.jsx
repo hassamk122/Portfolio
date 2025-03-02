7f852d46e7469f95cbc62c3c1c1428669513091a
hassamk122 <hassamkiani66@gmail.com>
2025-03-02 16:59:58 +0500

    removed scroll handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Header from './Components/Header/Header.jsx'
 import Sidebar from './Components/Sidebar/Sidebar.jsx';
-import ScrollHandler from './Pages/ScrollHandler.jsx';
 import Footer from './Components/Footer/Footer.jsx';
 import Home from './Pages/Home.jsx';
 import {Router,Link, Route,Routes } from 'react-router-dom';
 import Notfound from './Pages/Notfound.jsx';
 import Project from './Pages/Projects.jsx';
-import Experience from './Components/Experience/Experience.jsx';
 function App() {
 
   return (
@@ -14,7 +12,6 @@ function App() {
      
           <main className='ml-16 w-screen    dark:bg-zinc-900 bg-white'>
       <Header/>
-          <ScrollHandler/>
           <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Project />} />
