cbcb550ff210516a07a806b01557227fdf9ec459
hassamk122 <hassamkiani66@gmail.com>
2025-03-04 16:07:28 +0500

    fixed issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-
+import SmoothScroll from './Components/smoothScroll';
 const Header = lazy(() => import('./Components/Header/Header.jsx'));
 const Sidebar = lazy(() => import('./Components/Sidebar/Sidebar.jsx'));
 const Footer = lazy(() => import('./Components/Footer/Footer.jsx'));
@@ -9,7 +9,7 @@ const Home = lazy(() => import('./Pages/Home.jsx'));
 const Notfound = lazy(() => import('./Pages/Notfound.jsx'));
 const Project = lazy(() => import('./Pages/Projects.jsx'));
 const ScrollHandler = lazy(() => import('./Components/ScrollHandler.jsx'));
-const SmoothScroll =(import('./Components/SmoothScroll'));
+
 function App() {
   return (
     <div className="relative flex h-screen">
