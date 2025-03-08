a238ca930ed03253f59aac48ceef4eda2909f177
hassamk122 <hassamkiani66@gmail.com>
2025-03-08 12:11:33 +0500

    removed navigations from mobile

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -9,9 +9,9 @@ function Sidebar() {
         <ul className='flex items-center flex-col'>
             <li className='nav-item'><Link to='/' ><SideBarIcon icon={<Home  className='nav-icon'/>} text='Home' /></Link></li>
             <Divider/>
-           <li className='nav-item'><Link to='/#skills'><SideBarIcon icon={<Sparkles   className='nav-icon'/>} text='Skills'/></Link></li>
-            <li className='nav-item'><Link to='/#experience'><SideBarIcon icon={<Wand   className='nav-icon'/>} text="Experience" /></Link></li>
-            <li className='nav-item'><Link to='/#education'><SideBarIcon icon={<GraduationCap   className='nav-icon'/>} text='Education'/></Link></li>
+           <li className='nav-item hidden md:flex'><Link to='/#skills'><SideBarIcon icon={<Sparkles   className='nav-icon'/>} text='Skills'/></Link></li>
+            <li className='nav-item hidden md:flex'><Link to='/#experience'><SideBarIcon icon={<Wand   className='nav-icon'/>} text="Experience" /></Link></li>
+            <li className='nav-item hidden md:flex'><Link to='/#education'><SideBarIcon icon={<GraduationCap   className='nav-icon'/>} text='Education'/></Link></li>
             <li className='nav-item'><Link to='/projects' ><SideBarIcon icon={<Folder   className='nav-icon'/>} text="Projects" /></Link></li>
             <li className='nav-item'><Link to='/thumbnails' ><SideBarIcon icon={<Image   className='nav-icon'/>} text='Thumbnails'/></Link></li>
             <li className='nav-item'><Link to='/posters' ><SideBarIcon icon={<BookImage   className='nav-icon'/>} text='Posters'/></Link></li>
