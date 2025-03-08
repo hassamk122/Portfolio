ca4cd69a5357a0976b07e06acc76ebf82e939bb7
hassamk122 <hassamkiani66@gmail.com>
2025-03-08 12:17:48 +0500

    final touches

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Mail,MailCheck} from 'lucide-react'
+
 function Footer() {
   return (
     <>
@@ -7,7 +7,7 @@ function Footer() {
               <p className='text-teal-400 text-xs sm:text-sm dark:drop-shadow-[0px_2px_15px_rgba(0,213,190,0.3)] font-semibold '>Looking for a Designer?</p>
                 <p className='pt-1 text-[10px] sm:text-sm'>💌 Contact me at hassamkiani66@gmail.com.</p>
               </div>
-    <div className='w-full font-quicksand mt-10  bg-white/30 dark:bg-zinc-900/30 backdrop-blur-md border-t dark:border-t-zinc-100/5 border-t-zinc-200/30 p-2 overflow-hidden'>
+    <div className='flex font-quicksand items-center justify-center mt-10  border-t dark:border-t-zinc-100/5 border-t-zinc-200/30 w-full p-2 overflow-hidden'>
         <h5 className='flex items-center justify-center text-xs sm:text-sm dark:text-zinc-500'>© 2025 | Muhammad Hassam</h5>
        </div>
        </>
