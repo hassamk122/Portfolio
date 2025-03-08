8f88632cb36244b9a9697200aeb66e3d8515425b
hassamk122 <hassamkiani66@gmail.com>
2025-03-08 12:22:53 +0500

    final touches x2

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <>
+    <div>
     <div className='flex items-center justify-center flex-col mt-5 sm:mt-10 font-quicksand text-zinc-100 text-center'>
               <p className='text-teal-400 text-xs sm:text-sm dark:drop-shadow-[0px_2px_15px_rgba(0,213,190,0.3)] font-semibold '>Looking for a Designer?</p>
                 <p className='pt-1 text-[10px] sm:text-sm'>💌 Contact me at hassamkiani66@gmail.com.</p>
@@ -10,7 +10,7 @@ function Footer() {
     <div className='flex font-quicksand items-center justify-center mt-10  border-t dark:border-t-zinc-100/5 border-t-zinc-200/30 w-full p-2 overflow-hidden'>
         <h5 className='flex items-center justify-center text-xs sm:text-sm dark:text-zinc-500'>© 2025 | Muhammad Hassam</h5>
        </div>
-       </>
+       </div>
     )
 }
 
